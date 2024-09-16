3a09739ec980b06ff68164f9f777dec0a8808927
xiaochun <90xchun@gmail.com>
2024-09-16 09:56:00 +0800

    Fix logging patter error

--- src/main/java/com/zrlog/plugin/staticplus/Application.java
+++ src/main/java/com/zrlog/plugin/staticplus/Application.java
@@ -15,6 +15,9 @@ import java.util.List;
 
 public class Application {
 
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %5$s%6$s%n");
+    }
 
     /**
      * @param args
