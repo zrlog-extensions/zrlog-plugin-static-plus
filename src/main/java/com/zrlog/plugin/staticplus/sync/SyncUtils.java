64d677b03a478eebadf436f79ca97ec2d7d66bbf
xiaochun <90xchun@gmail.com>
2025-08-13 09:56:02 +0800

    Optimize sync

--- src/main/java/com/zrlog/plugin/staticplus/sync/SyncUtils.java
+++ src/main/java/com/zrlog/plugin/staticplus/sync/SyncUtils.java
@@ -4,6 +4,7 @@ import com.zrlog.plugin.IOSession;
 import com.zrlog.plugin.client.HttpClientUtils;
 import com.zrlog.plugin.common.LoggerUtil;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
@@ -20,7 +21,7 @@ public class SyncUtils {
         try {
             for (int i = 0; i < 40; i++) {
                 String gitBuildJson = checkUrl + "?_" + System.currentTimeMillis();
-                String body = HttpClientUtils.sendGetRequest(gitBuildJson, String.class, Map.of("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"), session);
+                String body = HttpClientUtils.sendGetRequest(gitBuildJson, String.class, Map.of("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"), session, Duration.ofSeconds(10));
                 if (Objects.equals(body, expectContent)) {
                     return true;
                 }
